a784d539d5ceff31d9a7073534d7a3b293370bc6
chizuko610 <yamamaya610@yahoo.co.jp>
2024-06-15 12:02:40 +0900

    New

---
+++ src/switch/rotationTypeLocalSwitch.cs
@@ -0,0 +1,28 @@
+﻿﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+//ゲームオブジェクトを順番にon/offするローカルスイッチ
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class rotationTypeLocalSwitch : UdonSharpBehaviour{
+	[Header("対象オブジェクト")]
+	[SerializeField] private GameObject[] target;
+	private int iActive=0;
+
+	public override void Interact(){
+		iActive++;
+		if(iActive>=target.Length){
+			iActive=0;
+		}
+
+		for(int i=0;i<target.Length;i++){
+			if(i==iActive){
+				target[i].SetActive(true);
+			}else{
+				target[i].SetActive(false);
+			}
+		}
+	}
+}
---
+++ src/switch/simpleIsLocalSwitch.cs
@@ -0,0 +1,23 @@
+﻿﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+//1つのゲームオブジェクトをon/offするローカルスイッチ
+//デフォルトのon/off状態は対象ブジェクトのデフォルト状態と同じになる。
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class simpleIsLocalSwitch : UdonSharpBehaviour{
+	[Header("対象オブジェクト")]
+	[SerializeField] private GameObject target;
+	private bool bActive;
+
+	public void Start(){
+		bActive=target.activeSelf;
+	}
+
+	public override void Interact(){
+		bActive=!bActive;
+		target.SetActive(bActive);
+	}
+}
